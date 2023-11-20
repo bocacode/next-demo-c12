c60e7f3ba1136ed4511d4c7172560d930006da1b
Todd H. Albert <todd@bocacode.com>
2023-11-20 12:25:58 -0500

    use next fetch

--- app/affirmations/old/page.js
+++ app/affirmations/old/page.js
@@ -18,7 +18,7 @@ export default function AffirmOld() {
       <h1 className="text-3xl font-bold dark:text-amber-200 text-amber-700">
         Old Affirmations
       </h1>
-      <section className="container mx-auto px-5 py-24 flex flex-wrap">
+      <section className="container mx-auto sm:px-5 px-2 py-8 flex flex-wrap">
         {affirmations && affirmations.map(item => (
           <AffirmationCard key={item.id} item={item} />
         ))}
